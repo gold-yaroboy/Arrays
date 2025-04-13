27ab57e9389f40b559b4577532153f350d0b4a92
ilyav <ilyav@ILIA>
2025-04-14 02:38:10 +0500

    Full

--- Shift/main.cpp
+++ Shift/main.cpp
@@ -11,15 +11,14 @@ void main()
 	int Array[SIZE] = {0,1,2,3,4,5,6,7,8,9};
 	for (int i = 0; i < SIZE; i++)cout << Array[i] << "\t";
 	cout << endl;
-	int temp = Array[SIZE - 1];
+	cout << endl;
+	int temp;
 	for (int j = 0; j < SIZE; j++)
 	{
-		for (int i = 0; i < SIZE; i++)
-		{
-			Array[i] = Array[i + 1];
-			Array[0] = temp;
-			cout << Array[i] << "\t";
-		}
+		temp = Array[SIZE - 1];
+		for (int i = SIZE - 1; i > 0; i--)Array[i] = Array[i - 1];
+		Array[0] = temp;
+		for (int i = 0; i < SIZE; i++)cout << Array[i] << "\t";
 		cout << endl;
 	}
 }
