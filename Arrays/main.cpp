4292fbc29cb275dbac68587fe4cbc9f23c854150
ilyav <ilyav@ILIA>
2025-04-14 00:43:22 +0500

    1.1, 1.2, 1.3, ... ni shift

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,16 +1,24 @@
 ﻿#include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int n = 5;
-	int arr[n] = { 3, 5, 8 };
-	arr[1] = 1024;
-	cout << arr[1] << endl;
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
+	
+	const int SIZE = 5;
+	cout << "Добрый вечер, для работы необходимо ввести исходные данные" << endl;
+	cout << "1.1)" << endl;
+	cout << "Перечислите 5 элементов массива на отдельных строках: " << endl;
+	int Array[SIZE] = {};
+	for (int i = 0;i < SIZE; i++)cin >> Array[i];
 	cout << endl;
+	cout << "1.2)" << endl;
+	for (int i = 0; i < SIZE; i++)cout << Array[i] << "\t";
+	cout << endl;
+	cout << endl;
+	cout << "1.3)" << endl;
+	for (int i = SIZE; i >= 1; i--) cout << Array[i] << "\t";
 }
--- Shift/main.cpp
+++ Shift/main.cpp
@@ -1,16 +1,14 @@
 ﻿#include<iostream>
 using namespace std;
-
+using std::cin;
+using std::cout;
+using std::endl;
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int n = 5;
-	int arr[n] = { 3, 5, 8 };
-	arr[1] = 1024;
-	cout << arr[1] << endl;
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
+	
+	const int SIZE = 10;
+	int Array[SIZE] = {0,1,2,3,4,5,6,7,8,9};
+	for (int i = 0; i < SIZE; i++)cout << Array[i] << "\t";
+	
 }
