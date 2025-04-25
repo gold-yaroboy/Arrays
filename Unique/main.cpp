98d3a29975ddfc609b7bd8afb128d5731ddf9a6f
ilyav <ilyav@ILIA>
2025-04-25 23:05:12 +0500

    unque done

--- Unique/main.cpp
+++ Unique/main.cpp
@@ -7,52 +7,29 @@ using std::endl;
 void main()
 {
     setlocale(LC_ALL, "");
-    const int n = 5;
-    int arr[n] = {};
-    int minRand, maxRand;
-    //range
-    do
+    const int n = 10;
+    int arr[n];
+    for (int i = 0; i < n;i++) //инициализация массива
     {
-        cout << "Введите минимальное случайное число: "; cin >> minRand;
-        cout << "Введите максимальное случайное число: "; cin >> maxRand;
-        if (minRand == maxRand)cout << "Числа должны быть разные" << endl;
-        if (minRand > maxRand)cout << "Минимальное должно быть больше" << endl;
-    } while (minRand >= maxRand);
-    /*if (maxRand < minRand)
-    {
-        int buffer = minRand;
-        minRand = maxRand;
-        maxRand = buffer;
-    }*/
-    //random_inicializing
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = rand() % (maxRand - minRand) + minRand; //ограничили снизу 50 сверху 100
-    }
-    //cout_array
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << "\t";
-    }
-    cout << endl;
-    cout << endl;
-    //sort_SELECTED
-    for (int i = 0; i < n; i++)//счётчик i выбирает элемент 
-    {
-        for (int j = i + 1; j < n; j++)
+        bool unique;
+        do
         {
-            if (arr[j] < arr[i])//счётчик j перебирает элементы
+            arr[i] = rand() % 10;
+            unique = true;
+            for (int j = 0; j < i;j++)
             {
-                int buf = arr[i];
-                arr[i] = arr[j];
-                arr[j] = buf;
-            }
-        }
+                if (arr[i] == arr[j])
+                {
+                    unique = false;
+                    break;
+                }
+            }            
+        } while (!unique);
     }
-    //cout_array
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) //вывод массива
     {
         cout << arr[i] << "\t";
     }
     cout << endl;
+
 }
