811f7b16672f18fc1b8cda78c064a8202306a774
ilyav <ilyav@ILIA>
2025-04-26 00:08:08 +0500

    2d sort done

--- 2Darrays/main.cpp
+++ 2Darrays/main.cpp
@@ -11,10 +11,17 @@ void main()
 	int arr[ROWS][COLS] = 
 	{ 
 		{ 1, 2, 3 }, 
-		{ 5, 6, 7 }, 
-		{ 9, 10, 11 }, 
+		{ 4, 5, 6 }, 
+		{ 7, 8, 9 }, 
 	};
-	for (int i = 0; i < ROWS; i++)
+	for (int i = 0; i < ROWS; i++)//заполнение рандом
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}		
+	}
+	for (int i = 0; i < ROWS; i++)//вывод исходного массива
 	{
 		for (int j = 0; j < COLS; j++)
 		{
@@ -22,4 +29,33 @@ void main()
 		}
 		cout << endl;
 	}
+	cout << endl;
+	//сортировка массива
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			for (int k = i; k < ROWS;k++)
+			{
+				for (int l = k > i ? 0 : j + 1; l < COLS; l++)
+				{
+					if (arr[k][l] < arr[i][j])
+					{
+						int buffer = arr[i][j];
+						arr[i][j] = arr[k][l];
+						arr[k][l] = buffer;
+					}					
+				}
+			}
+		}
+	}
+	for (int i = 0; i < ROWS; i++)//вывод отсортированного массива
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << endl;
 }
