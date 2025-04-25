e7c2010c51c481d9bddb51220c31da17309bb04a
ilyav <ilyav@ILIA>
2025-04-25 22:49:07 +0500

    numerics

--- Numerics/main.cpp
+++ Numerics/main.cpp
@@ -7,14 +7,15 @@ using std::endl;
 void main()
 {
     setlocale(LC_ALL, "");
-
     int num;
+    
     cout << "Введите число, а я сним что нибудь придумаю: "; 
     cin >> num; cout << endl;
+    int nu = num;
 
     int binary[32] = { 0 };
     int i = 0;
-    for (i = 0; num > 0; i++) 
+    for (; num > 0; i++) 
     {
         binary[i] = num % 2;
         num /= 2;
@@ -24,5 +25,15 @@ void main()
     else for (int j = i - 1; j >= 0; j--)cout << binary[j];
     cout << endl;
 
-    //Шестнадцатиричное не выкупаю...
+    const int n = 8;
+    char hex[n] = {};
+    int l = 0;
+    for (l;nu;nu >>= 4)
+    {
+        hex[l] = nu & 15;
+        hex[l++] += hex[l] < 10 ? 48 : 55;
+    }
+    cout << "Шестнадцатиричное: ";
+    for (--l;l >= 0;l--)cout << hex[l];
+    cout << endl;
 }
